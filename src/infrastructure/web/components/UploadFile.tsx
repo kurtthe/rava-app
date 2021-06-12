813c346b85e5475b34e04c0908e76d01f0210f42
Matheo Nieto <matheo.developer@gmail.com>
2021-06-12 12:47:57 -0500

    cove upload

--- src/infrastructure/web/components/UploadFile.tsx
+++ src/infrastructure/web/components/UploadFile.tsx
@@ -1,40 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import ProfileImage from "../assets/img/profile.png";
+import ProfileCover from "../assets/img/profile-cover.png";
 import "../assets/styles/upload-file.scss";
 
 import { AntDesign } from "react-web-vector-icons";
 
 interface Props {
+  type: "simple" | "cover";
   label: string;
   titleInfo?: string;
   detailsInfo?: string;
+  onChangeFile?: () => void;
 }
 
 const UploadFile = (props: Props) => {
+  const hiddenFileInput = useRef(null);
   const [imageUpload, setImageUpload] = useState("");
 
-  return (
-    <>
-      <div className="upload">
-        <div className="upload__image">
+  const handleClick = () => {
+    if (null != hiddenFileInput.current) {
+      hiddenFileInput.current.click();
+    }
+  };
+
+  const handleChangeFile = (event) => {
+    const urlImage = URL.createObjectURL(event.target.files[0]);
+
+    console.log("event.target.files", urlImage);
+    setImageUpload(urlImage);
+
+    props.onChangeFile && props.onChangeFile();
+  };
+
+  const putContent = () => {
+    if (props.type == "simple") {
+      return (
+        <>
+          <div className={`upload-${props.type}__image`}>
+            <img
+              src={imageUpload != "" ? imageUpload : ProfileImage}
+              alt="Image Profile"
+              width="116px"
+              height="116px"
+            />
+          </div>
+          <div className={`upload-${props.type}__content`}>
+            {!props.titleInfo ? null : (
+              <p className={`upload-${props.type}__title`}>{props.titleInfo}</p>
+            )}
+            {!props.detailsInfo ? null : (
+              <p className={`upload-${props.type}__details`}>{props.detailsInfo}</p>
+            )}
+            <input ref={hiddenFileInput} type="file" onChange={handleChangeFile} />
+            <button onClick={handleClick} className={`upload-${props.type}__btn`}>
+              <AntDesign name="plus" color="#1F2044" size={20} />
+              {props.label}
+            </button>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        {!props.titleInfo ? null : (
+          <p className={`upload-${props.type}__title`}>{props.titleInfo}</p>
+        )}
+
+        <div className={`upload-${props.type}__image`}>
           <img
-            src={imageUpload != "" ? imageUpload : ProfileImage}
+            src={imageUpload != "" ? imageUpload : ProfileCover}
             alt="Image Profile"
-            width="116px"
+            width="436px"
             height="116px"
           />
         </div>
-        <div className="upload__content">
-          {!props.titleInfo ? null : <p className="upload__title">{props.titleInfo}</p>}
-          {!props.detailsInfo ? null : <p className="upload__details">{props.detailsInfo}</p>}
-          <input id="inputFile" type="file" />
-          <label htmlFor="inputFile" className="upload__btn">
-            <AntDesign name="plus" color="#1F2044" size={20} />
-            {props.label}
-          </label>
-        </div>
-      </div>
+
+        {!props.detailsInfo ? null : (
+          <p className={`upload-${props.type}__details`}>{props.detailsInfo}</p>
+        )}
+
+        <input ref={hiddenFileInput} type="file" onChange={handleChangeFile} />
+        <button onClick={handleClick} className={`upload-${props.type}__btn`}>
+          <AntDesign name="plus" color="#1F2044" size={20} />
+          {props.label}
+        </button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className={`upload-${props.type}`}>{putContent()}</div>
     </>
   );
 };
--- src/infrastructure/web/pages/profile/Profile.tsx
+++ src/infrastructure/web/pages/profile/Profile.tsx
@@ -18,9 +18,17 @@ const Profile = (props: Props) => {
           <HeaderMini description="Hey Mate, Welcome! Please complete your profile picture to continue to login" />
           <div className="profile__content">
             <UploadFile
+              type="simple"
               label="Upload picture"
               titleInfo="Add your profile photo"
               detailsInfo="Add your photo for Trainer Account"
+            />
+
+            <UploadFile
+              type="cover"
+              label="Upload picture"
+              titleInfo="Add your cover photo"
+              detailsInfo="Add cover photo for Trainer Account (min. 2mb)"
             />
           </div>
         </div>
